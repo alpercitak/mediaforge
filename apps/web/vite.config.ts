2b332bb1ea42a0b54bc00bcb2fc9bf5009da9511
Alper Çıtak <citak.alper@gmail.com>
2026-05-05 01:14:24 +0200

    update font

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
+import webfontDownload from 'vite-plugin-webfont-dl';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
+const INTER_FONT_URL = 'https://fonts.googleapis.com/css2?family=Inter:wght@400..600&display=swap' as const;
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), webfontDownload([INTER_FONT_URL])],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
